03ae09637fea0dd294dc803f545387c45edf47bf
DelandHan <neil.deland@hotmail.com>
2016-10-28 20:31:08 +0800

    it

--- MiniXMLEditor/ConsoleApplication2/ConsoleApplication2.cpp
+++ MiniXMLEditor/ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,10 +2,82 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+class Test
+	:public ILine
+{
+public:
+	Test() {
+		theNode = nullptr;
+	}
+	~Test() {
+	}
+
+	void setNode(xml::XMLNode *node) {
+		theNode = node;
+	}
+
+
+	int expand(Page * dest) { 	
+		xml::XMLNode *node = theNode->getFirstChild();
+		
+		while (node)
+		{
+			dest->add<Test>()->setNode(node);
+			node = node->getNext();
+		}
+		return 0;
+	}
+	int getMyPage(Page * dest) { return 0; }
+
+	std::wstring getStr() { 
+		std::wstring buff;
+		buff.resize(theNode->getString().size());
+		std::copy(theNode->getString().begin(), theNode->getString().end(), buff.begin());
+		return buff; 
+	}
+	int getFormat() { return 0; }
+
+private:
+	xml::XMLNode * theNode;
+};
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	xml::XMLParser ps;
+	ps.parseFile("IECU.xml");
+	xml::XMLNode *node = ps.pickupDocument();
+
+	Test seed;
+	seed.setNode(node);
+
+	ILine::Page page, page2;
+
+	seed.expand(&page);
+
+	ILine *body = 0;
+	for (ILine::Page::Range r = page.getAll(); r.first != r.second; r.first++)
+	{
+		std::cout << "1. ";
+		std::wstring bu = (*r.first)->getStr();
+		if (bu[0] == L'A') {
+			body = *(r.first);
+			body->expand(&page2);
+		}
+	}
+
+	for (ILine::Page::Range r = page2.getAll(); r.first != r.second; r.first++)
+	{
+		std::wcout << (r.first).operator*()->getStr() << std::endl;
+	}
+
+
+	delete node;
+
+	system("pause");
     return 0;
 }
 
--- MiniXMLEditor/ConsoleApplication2/Interfaces.h
+++ MiniXMLEditor/ConsoleApplication2/Interfaces.h
@@ -16,10 +16,34 @@ class ILine
 public:
 	virtual ~ILine() = default;
 
-	typedef std::list<ILine*> Page;
+	class Page
+	{
+	public:
+		typedef std::pair<std::list<ILine*>::iterator, std::list<ILine*>::iterator> Range;
+
+		Page() = default;
+		~Page() { clear(); }
+
+		void clear() {
+			for (auto it : thePage) delete it;
+			thePage.clear();
+		}
+
+		Range getAll() { return Range(thePage.begin(), thePage.end()); }
+
+		template<typename T> 
+		typename std::enable_if<std::is_base_of<ILine,T>::value, T*>::type add() {
+			T * fresh = new T;
+			thePage.push_back(fresh);
+			return fresh;
+		}
+
+	private:
+		std::list<ILine*> thePage;
+	};
 	
+	virtual int expand(Page * dest) = 0;
 	virtual int getMyPage(Page * dest) = 0;
-	virtual int expandMe(Page * dest) = 0;
 
 	virtual std::wstring getStr() = 0;
 	virtual int getFormat() = 0;
